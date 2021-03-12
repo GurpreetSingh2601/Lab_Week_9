95301f0de223b7e5b46a77ffc7d01b93c0a4b09b
Gurpreet Singh <gurubahre2019@gmail.com>
2021-03-12 12:23:47 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 10;
+const s = 'Hello This is a sample output string.';
+const t = true;
+
+if (a === b) {
+    console.log('they match.')
+}
+else {
+    console.log('no luck.')
+}
+
+for (const value of s) {
+    console.log(value);
+}
